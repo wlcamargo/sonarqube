1e74d0a060d5c66a7acad7a593ce0ded3faa300c
wallace <wallacecpg@gmail.com>
2024-08-12 15:51:41 +0100

    initial commit

---
+++ tests/test_ops.py
@@ -0,0 +1,16 @@
+import pytest
+from ops import add,substract,multiply,divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+def test_divide():
+    assert divide(10, 5) == 2
+
+def test_substract():
+    assert substract(10, 5) == 5
+
+
